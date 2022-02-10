5a13f715675f9d6a1874493a73d9d564a9f80421
Eddie Naff <eddie@eddienaff.dev>
2022-02-09 17:40:12 -0800

    Ditch ts-node

--- scripts/move-index.ts
+++ scripts/move-index.ts
@@ -1,6 +1,10 @@
 import fse from "fs-extra";
-import { resolve } from "path";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
 const oldPath = resolve(__dirname, "../", "src/index.html");
 const newPath = resolve(__dirname, "../", "dist/index.html");
 
