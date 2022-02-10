b93ab39671b55ad306a13f2b15b942202e1d2f81
Eddie Naff <eddie@eddienaff.dev>
2022-02-09 17:17:17 -0800

    Add module to append cells to the DOM

---
+++ src/draw-dungeon.ts
@@ -0,0 +1,31 @@
+import { createDungeon } from "./main.js";
+
+let store = {
+  dungeon: createDungeon(),
+};
+
+const rows = store.dungeon.map((item, index) => {
+  const row = document.createElement("div");
+  row.className = `row`;
+  row.id = `row-${index}`;
+
+  const newCell = item.map(({ opacity, type }) => {
+    const cell = document.createElement("div");
+
+    cell.className = "cell";
+    cell.textContent = `I am div # ${index}`;
+    cell.style.opacity = `${opacity}`;
+    return cell;
+  });
+  row.append(...newCell);
+
+  return row;
+});
+
+const element = document.getElementById("dungeon");
+
+if (!element) {
+  throw new Error("Cannot find DOM element 'dungeon'");
+}
+
+element.append(...rows);
