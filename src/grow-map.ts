27c856dc1c5535a96c10b7d4afeebf6a7de5b179
Eddie Naff <eddie@eddienaff.dev>
2022-02-13 07:26:04 -0800

    Add helper modules

---
+++ src/grow-map.ts
@@ -0,0 +1,34 @@
+import {
+  GridSquare,
+  gridSettings,
+  Room,
+  GridAndRooms,
+} from "./create-dungeon.js";
+import { createRoomsFromSeed } from "./create-room-from-seed.js";
+
+export function growMap(
+  grid: GridAndRooms,
+  seedRooms: Room[],
+  counter = 1,
+  maxRooms = gridSettings.MAX_ROOMS
+): GridAndRooms {
+  if (!seedRooms) {
+    // return { grid, placedRooms: [] };
+    return grid;
+  }
+  if (counter + seedRooms.length > maxRooms || !seedRooms.length) {
+    // return { grid, placedRooms: [] };
+    return grid;
+  }
+
+  const poppedSeed = seedRooms.pop();
+
+  if (!poppedSeed) {
+    throw new Error("The seeded room is missing!");
+  }
+
+  grid = createRoomsFromSeed(grid, poppedSeed);
+  seedRooms.push(...grid.placedRooms);
+  counter += grid.placedRooms.length;
+  return growMap(grid, seedRooms, counter);
+}
