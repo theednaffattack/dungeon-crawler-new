2bfb46b0bdc3d7895f2aea7d9ca59485608db008
Eddie Naff <eddie@eddienaff.dev>
2022-06-10 11:47:01 -0700

    Add pacman to nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 export function Nav() {
   return (
     <nav>
-      <Link to="/">Home</Link> | <Link to="static-dungeon">Static Dungeon</Link>
+      <Link to="/">Home</Link> | <Link to="static-dungeon">Static Dungeon</Link>{" "}
+      | <Link to="pacman">Pacman</Link>
     </nav>
   );
 }
