2deea554748063988bf6be410c1d12d788aa6102
Eddie Naff <eddie@eddienaff.dev>
2022-06-25 12:33:11 -0700

    Add notes to clamp function

--- src/pacman/clamp.ts
+++ src/pacman/clamp.ts
@@ -1,4 +1,11 @@
 // Clamp number between two values with the following line:
+
+/**
+ * Returns a supplied numeric expression constrained between the min and max value
+ * @param num The value to be constrained.
+ * @param min The minimum number allowed.
+ * @param max The maximum number allowed.
+ */
 export function clamp(num: number, min: number, max: number) {
   return Math.min(Math.max(num, min), max);
 }
